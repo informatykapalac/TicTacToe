29e8b58ac9b8387d9dafc91e63e8688cd2f68160
_jrw <_jrw@pc104.home>
2019-01-05 20:49:21 +0100

    Poprawki

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import Game from './GameComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <Game/>
     );
   }
 }
